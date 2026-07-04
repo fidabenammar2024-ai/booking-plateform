e649b6f6ec3ab260ff7cb5fbce541226d70c9f2f
kali <kali@kali.kali>
2026-07-04 15:08:04 +0100

    adding the code of index.php file

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+5<meta charset="UTF-8">
+<title>Booking Platform</title>
+</head>
+<body>
+<h1>Bienvenue sur la plateforme de réservation sportive</h1>
+<nav>
+<a href="register.php">Inscription</a> |
+<a href="login.php">Connexion</a>
+</nav>
+</body>
+</html>
