3d9bed139680249795993ba6dfc6632772fc5010
kali <kali@kali.kali>
2026-07-05 10:26:23 +0100

    modification dashboard.php

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,15 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+header("Location: login.php");
+exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 <meta charset="UTF-8">
 <title>Tableau de bord</title>
+<link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
+<div class="container">
 <h1>Tableau de bord</h1>
-<p>Bienvenue sur ton espace utilisateur.</p>
+<p>Bienvenue, <?php echo htmlspecialchars($_SESSION["user_name"]); ?>.</p>
 <nav>
-<a href="index.php">Accueil</a> |
-<a href="login.php">Deconnexion</a>
+<a href="index.php">Accueil</a>
+<a href="fields.php">Terrains</a>
+<a href="logout.php">Deconnexion</a>
 </nav>
+</div>
 </body>
 </html>
